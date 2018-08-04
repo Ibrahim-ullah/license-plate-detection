f5ac8d301bf1b69a89193878634c57f41649322b
Ibrahim Ullah <34267927+Ibrahim-ullah@users.noreply.github.com>
2018-08-04 21:53:40 +0600

    Update ocrtest.py

--- converting-image-to-string/ocrtest.py
+++ converting-image-to-string/ocrtest.py
@@ -3,4 +3,5 @@ import pytesseract
 from PIL import Image
 print pytesseract.image_to_string(Image.open('pic.png'))
 
-#to run the code you have to go to the terminal and type tesseract pic.png out
+#to run the code you have to go to the terminal and type tesseract pic.png out.Here pic.png is the name of your picture and out indicates the 
+#text file name where you are gonna save the string.
